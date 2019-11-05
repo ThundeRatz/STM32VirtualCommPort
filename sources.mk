52ce9a931a6f888b7ff11aefb88312267e91a186
Lucas Schneider <lucastrschneider@gmail.com>
2019-11-05 14:18:14 -0300

    Update makefile paths

--- sources.mk
+++ sources.mk
@@ -1,11 +1,16 @@
 THIS_PATH := $(patsubst %/,%,$(dir $(abspath $(lastword $(MAKEFILE_LIST)))))
 
-C_INCLUDES +=                                                          \
-	-I$(THIS_PATH)/core/inc                                            \
-	-I$(THIS_PATH)/platform/inc                                        \
-    -I$(THIS_PATH)/inc                                                 \
+C_INCLUDES +=                                                               \
+	-I$(THIS_PATH)/Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Inc    \
+	-I$(THIS_PATH)/Middlewares/ST/STM32_USB_Device_Library/Core/Inc         \
+    -I$(THIS_PATH)/inc                                                      \
 
-LIB_SOURCES += $(shell find $(THIS_PATH)/core/src $(THIS_PATH)/platform/src $(THIS_PATH)/src -name "*.c")
+# C_INCLUDE += $(addprefix -I, $(sort $(dir $(shell find $(THIS_PATH) -name "*.h")))
 
+LIB_SOURCES += $(shell find                                             \
+    $(THIS_PATH)/Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Src  \
+    $(THIS_PATH)/Middlewares/ST/STM32_USB_Device_Library/Core/Src       \
+    $(THIS_PATH)/src -name "*.c"                                        \
+)
 
 undefine THIS_PATH
